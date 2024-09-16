8ed5d203c4853fd3470b58770f1189dcdfb8fe9f
altunf <altunnfirat@gmail.com>
2024-09-16 15:02:52 +0300

    feat: added new statistical data to info-card-list

--- client/components/info-card-list.jsx
+++ client/components/info-card-list.jsx
@@ -4,26 +4,37 @@ import React, { useEffect, useState } from "react";
 import { useUniversityContext } from "@/context/university-context";
 
 export const InfoCardList = () => {
-  const { mergedData } = useUniversityContext();
+  const { mergedData, academics } = useUniversityContext();
   const [englishCount, setEnglish] = useState();
+  const [accreditationCount, setAccreditation] = useState();
 
   useEffect(() => {
     const english = mergedData.filter((el) => el.undergraduate.english == true);
     setEnglish(english.length);
+
+    const accreditation = mergedData.filter(
+      (el) => el.accreditation !== "Hayır"
+    );
+    setAccreditation(accreditation.length);
   }, [mergedData]);
 
   const titles = [
     "Toplam Üniversite Sayısı",
     "Akademisyen Sayısı",
-    "Vakıf Üniversitesi Sayısı",
-    "Vakıf Üniversitesi Sayısı",
+    "Eğitim Dili İngilizce Olan Üniversiteler",
+    "Akreditasyonu Olan Üniversite Sayısı",
   ];
-  const values = [145, 1355, englishCount, 125];
+  const values = [
+    mergedData.length,
+    academics.length,
+    englishCount,
+    accreditationCount,
+  ];
   const description = [
     "Psikoloji bölümü bulunan üniversiteler",
     "Psikoloji bölümlerinde yer alan akademisyenler",
-    "Psikoloji bölümü bulunan vakıf üniversiteleri",
-    "Psikoloji bölümü bulunan vakıf üniversiteleri",
+    "Psikoloji bölümünün eğitim dili İngilizce olan üniversiteler",
+    "Psikoloji bölümü akredite olan üniversite sayısı",
   ];
 
   return (
