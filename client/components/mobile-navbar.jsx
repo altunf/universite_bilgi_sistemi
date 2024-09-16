884d955f9b8bf7246088c267b46ce23648e96d31
altunf <altunnfirat@gmail.com>
2024-09-16 22:06:23 +0300

    feat: created mobile-sheet component and add sheet menu into mobile-navbar

---
+++ client/components/mobile-navbar.jsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { Menu, School } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+export const MobileNavbar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">Toggle navigation menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <nav className="grid gap-6 text-lg font-medium">
+          <Link
+            href="/"
+            className="flex items-center gap-2 text-lg font-semibold"
+          >
+            <School className="h-6 w-6" />
+            <span className="sr-only">Anasayfa</span>
+          </Link>{" "}
+          <Link
+            href="/"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            Anasayfa
+          </Link>
+          <Link
+            href="/universities"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            Üniversiteler
+          </Link>
+          <Link
+            href="/academics"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            Akademisyenler
+          </Link>
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
